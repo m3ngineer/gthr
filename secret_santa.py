7f60e215561c78cc358066c0b62b232a6bf68c6e
Matthew Eng <matthew.eng2@gmail.com>
2024-12-18 22:11:10 -0500

    Revise for 2024; add template for configuration

--- secret_santa.py
+++ secret_santa.py
@@ -1,4 +1,4 @@
-''' Pulls names for Secret Santa! '''
+""" Pulls names for Secret Santa! """
 
 import random
 import smtplib, ssl
@@ -9,35 +9,40 @@ from collections import Counter
 
 import secret_santa_config
 
+
 def send_email(from_, to_, username, password, subject, body):
 
     port = 465  # For SSL
 
     # Create a secure SSL context
-    context = ssl.create_default_context()
+    # context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    with smtplib.SMTP_SSL("smtp.gmail.com", port) as server:
         server.login(username, password)
-        print('Logged into server..')
+        print("Logged into server..")
 
         msg = MIMEMultipart()
         msg["From"] = from_
         msg["To"] = to_
         msg["Subject"] = subject
-        msg.attach(MIMEText(body, 'html'))
+        msg.attach(MIMEText(body, "html"))
 
         server.sendmail(from_, to_, msg.as_string())
         server.quit()
 
+
 def pull_names(secret_santa_config_file):
-    '''
+    """
     secret_santa_config_file: path to file that contains names, emails, and a list of
     pairs that should not be matched
-    '''
+    """
 
     # Choose secret santa pairs
     restart = True
     while restart:
+        invalid_matches = [
+            sorted(match) for match in secret_santa_config_file.invalid_matches
+        ]
         gifter_names = list(secret_santa_config_file.names.keys())
         receiver_names = list(secret_santa_config_file.names.keys())
         pairs = []
@@ -45,17 +50,25 @@ def pull_names(secret_santa_config_file):
         for gifter in gifter_names:
             restart = False
             check_pairing = True
+
             while check_pairing:
                 receiver = random.choice(receiver_names)
-                if gifter != receiver and set([gifter,receiver]) not in secret_santa_config_file.invalid_matches:
+                if (
+                    gifter != receiver
+                    and sorted([gifter, receiver]) not in invalid_matches
+                ):
                     receiver_names.remove(receiver)
                     pairs.append((gifter, receiver))
                     check_pairing = False
-                elif len(receiver_names) == 1: # if only person left has themselves to pick
+                elif (
+                    len(receiver_names) == 1
+                ):  # if only person left has themselves to pick
                     # Start over
                     restart = True
                     check_pairing = False
-                elif (len(receiver_names) == 2) and (set(receiver_names) in secret_santa_config_file.invalid_matches):
+                elif (len(receiver_names) == 2) and (
+                    sorted(receiver_names) in invalid_matches
+                ):
                     # Only remaining names cannot be matched
                     # Start over
                     restart = True
@@ -64,38 +77,48 @@ def pull_names(secret_santa_config_file):
                     continue
 
     # Check pairs
-    check_pairs = Counter([j for i in [list(pair) for pair in pairs] for j in i]).values()
+    check_pairs = Counter(
+        [j for i in [list(pair) for pair in pairs] for j in i]
+    ).values()
     pair_warning = any([False if i <= 2 else True for i in check_pairs])
-    multiple_names_warning1 = sorted(secret_santa_config.names.keys()) != sorted([i[0] for i in pairs])
-    multiple_names_warning2 = sorted(secret_santa_config.names.keys()) != sorted([i[1] for i in pairs])
+    multiple_names_warning1 = sorted(secret_santa_config.names.keys()) != sorted(
+        [i[0] for i in pairs]
+    )
+    multiple_names_warning2 = sorted(secret_santa_config.names.keys()) != sorted(
+        [i[1] for i in pairs]
+    )
     if pair_warning:
-        print('Repick names!!')
+        print("Repick names!!")
 
     elif multiple_names_warning1 or multiple_names_warning2:
-        print('Repick names!!')
+        print("Repick names!!")
 
     else:
         # Send emails
-        subject = 'The 2022 Virtual Holiday Cookie Bake-off!'
+        subject = "The 2024 Virtual Holiday Cookie Bake-off!"
 
         for pair in pairs:
             gifter = pair[0]
             receiver = pair[1]
 
-            file = open('secret_santa_msg.html', 'r')
+            file = open("cookie_bakeoff_msg.html", "r")
             msg = file.read()
             msg = msg.format(gifter, receiver)
 
             gifter_email = secret_santa_config_file.names[gifter]
-            send_email(from_=secret_santa_config_file.email['username'],
+            send_email(
+                from_=secret_santa_config_file.email["username"],
                 to_=gifter_email,
-                username=secret_santa_config_file.email['username'],
-                password=secret_santa_config_file.email['password'],
-                subject=subject, body=msg)
+                username=secret_santa_config_file.email["username"],
+                password=secret_santa_config_file.email["password"],
+                subject=subject,
+                body=msg,
+            )
 
-        print('Emails sent.')
+        print("Emails sent.")
 
     return
+
 
 if __name__ == "__main__":
 
---
+++ secret_santa_config_template.py
@@ -0,0 +1,8 @@
+names = {
+    "Name1": "email1@gmail.com",
+    "Name2": "email2@gmail.com",
+}
+
+invalid_matches = [["Name1", "Name2"]]
+
+email = {"username": "users@gmail.com", "password": "password"}
